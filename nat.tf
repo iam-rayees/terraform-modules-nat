115022fbcf1b2d2b10bd89649c8ccb975c9ba279
rayees <mailtomdrayees@gmail.com>
2026-02-05 12:55:45 +0530

    Updated nat infra

--- nat.tf
+++ nat.tf
@@ -7,6 +7,6 @@ resource "aws_nat_gateway" "natgw" {
   subnet_id     = var.public_subnet_id
 
   tags = {
-    Name = "my-NAT-GW"
+    Name = "${environment}-NAT-GW"
   }
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,5 @@ variable "public_subnet_id" {
 variable "private_route_table_id" {
   type = string
 }
+
+variable "environment" {}
