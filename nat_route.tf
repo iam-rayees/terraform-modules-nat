88e702c60c7c69449a37a79927927ec5b2dc4b76
rayees <mailtomdrayees@gmail.com>
2026-02-05 13:18:38 +0530

    Updated nat infra

---
+++ nat_route.tf
@@ -0,0 +1,5 @@
+resource "aws_route" "private_nat_route" {
+  route_table_id         = var.private_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.natgw.id
+}
